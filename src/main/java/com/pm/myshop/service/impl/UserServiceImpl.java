468545243c4ad000ed12974d0feff241dcc13798
Kundan <kundanparajuli@gmail.com>
2014-12-30 15:29:59 -0600

    Merge origin/master
    Conflicts:
    	faces-config.NavData
    	src/main/webapp/resources/css/jsfcrud.css

---
+++ src/main/java/com/pm/myshop/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pm.myshop.service.impl;
+
+import com.pm.myshop.service.UserService;
+
+/**
+ *
+ * @author kunda_000
+ */
+public class UserServiceImpl implements UserService {
+    
+}
